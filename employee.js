226231d20b475454c6b4a6ba1558bc3e08e1e138
Aadam Ali <aadamali708@gmail.com>
2024-02-03 11:20:35 +0000

    finished bind task

--- employee.js
+++ employee.js
@@ -1,1 +1,20 @@
 // Your code here
+class Employee {
+
+    constructor(name, occupation){
+        this.name = name;
+        this.occupation = occupation;
+
+    }
+
+    sayName(){
+
+        console.log(`Hello ${this.name}`);
+    }
+
+    sayOccupation(){
+        console.log(`${this.name} is a ${this.occupation}`);
+    }
+}
+
+module.exports = Employee;
--- test.js
+++ test.js
@@ -1,1 +1,8 @@
 // Your code here
+const Employee = require(`./employee`);
+
+const e1 = new Employee('dave', 'slave');
+
+const occ = e1.sayOccupation.bind(e1);
+
+global.setTimeout(occ,3000);
