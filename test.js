91c865a8a3d49737b5584384fe50aa07f29e57a9
Aadam Ali <aadamali708@gmail.com>
2024-02-03 11:21:30 +0000

    finished bind task

--- test.js
+++ test.js
@@ -1,7 +1,7 @@
 // Your code here
 const Employee = require(`./employee`);
 
-const e1 = new Employee('dave', 'slave');
+const e1 = new Employee('john wick', 'dog walker');
 
 const occ = e1.sayOccupation.bind(e1);
 
